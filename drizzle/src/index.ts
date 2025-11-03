8c5df8bd81576b5ca8b87993303f2876f8bc1857
Movieguy3333 <Movieguy3333@gmail.com>
2025-11-03 14:03:47 -0500

    add drizzle

--- drizzle/src/index.ts
+++ drizzle/src/index.ts
@@ -1,8 +1,8 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
-import * as schema from "./db/schema.ts";
+import * as schema from "./db/schema";
 import dotenv from "dotenv";
-import { eq } from 'drizzle-orm';
+import { eq } from "drizzle-orm";
 
 dotenv.config();
 
@@ -11,7 +11,6 @@ const pool = new Pool({
 });
 
 export const db = drizzle(pool, { schema });
-
 
 //functions to interact with the database
 export async function getAllUsers() {
@@ -24,14 +23,24 @@ export async function createUser(data: { name: string }) {
 }
 
 export async function deleteUser(username: string) {
-  return await db.delete(schema.users).where(eq(schema.users.username, username));
+  return await db
+    .delete(schema.users)
+    .where(eq(schema.users.username, username));
 }
 
 export async function getUser(username: string) {
-  return await db.select().from(schema.users).where(eq(schema.users.username, username)).limit(1);
+  return await db
+    .select()
+    .from(schema.users)
+    .where(eq(schema.users.username, username))
+    .limit(1);
 }
 
-export async function logFoodEntry(data: { userID: string; foodID: string; servingSize: number }) {
+export async function logFoodEntry(data: {
+  userID: string;
+  foodID: string;
+  servingSize: number;
+}) {
   return await db.insert(schema.foodLog).values({
     userID: data.userID,
     foodID: data.foodID,
